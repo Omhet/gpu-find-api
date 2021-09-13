0add2a225e651b5682c57af684f447e945d23b6e
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-09-13 11:48:52 +0300

    add more cards

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,12 @@
 export const shopUrl = 'https://www.e-katalog.ru';
 
+export const CardsPaths3060 = [
+    'PALIT-GEFORCE-RTX-3060-DUAL.htm',
+    'PALIT-GEFORCE-RTX-3060-STORMX.htm',
+    'PALIT-GEFORCE-RTX-3060-DUAL-OC.htm',
+    'PALIT-GEFORCE-RTX-3060-STORMX-OC.htm',
+];
+
 export const CardsPaths3070 = [
     'PALIT-GEFORCE-RTX-3070-GAMEROCK.htm',
     'PALIT-GEFORCE-RTX-3070-GAMINGPRO.htm',
@@ -9,14 +16,21 @@ export const CardsPaths3070 = [
     'PALIT-GEFORCE-RTX-3070-JETSTREAM-OC.htm',
 ];
 
-export const CardsPaths3060 = [
-    'PALIT-GEFORCE-RTX-3060-DUAL.htm',
-    'PALIT-GEFORCE-RTX-3060-STORMX.htm',
-    'PALIT-GEFORCE-RTX-3060-DUAL-OC.htm',
-    'PALIT-GEFORCE-RTX-3060-STORMX-OC.htm',
+export const CardsPaths3080 = [
+    'PALIT-GEFORCE-RTX-3080-GAMEROCK.htm',
+    'PALIT-GEFORCE-RTX-3080-GAMINGPRO.htm',
+    'PALIT-GEFORCE-RTX-3080-GAMINGPRO-OC.htm',
+];
+
+export const CardsPaths3090 = [
+    'PALIT-GEFORCE-RTX-3090-GAMEROCK.htm',
+    'PALIT-GEFORCE-RTX-3090-GAMINGPRO.htm',
+    'PALIT-GEFORCE-RTX-3090-GAMINGPRO-OC.htm',
 ];
 
 export const CardsPaths = {
     '3060': CardsPaths3060,
     '3070': CardsPaths3070,
+    '3080': CardsPaths3080,
+    '3090': CardsPaths3090,
 };
